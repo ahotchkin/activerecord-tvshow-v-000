44b1a2c83a63ffafefa61f6bdb987534854e78a6
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-09-22 20:40:15 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,7 +7,7 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show
     # Show.where(self.highest_rating).take
-    Show.order(rating: :desc)
+    Show.order(rating: :desc).take
     binding.pry
   end
 
